47f27add253984b7ca8b635739aa390aabd372aa
BobDaGithubAccount <62474433+BobDaGithubAccount@users.noreply.github.com>
2024-08-11 16:05:43 +0100

    Deployment hotfix

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     client_6: './src/client_6.ts',
     client_7: './src/client_7.ts',
     client_8: './src/client_8.ts',
+    client_8: './src/client_cfd.ts',
     client_lagrangian: './src/client_lagrangian.ts'
   },
   output: {
